3c427c250c23fbfa5497abfd0a5c62933b6d37c1
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-11-04 15:52:58 +0300

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import {View, Text, Button, SafeAreaView, FlatList} from 'react-native';
-import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Card from './components/Card';
 
+const URL = 'https://jsonplaceholder.typicode.com/users';
+
 const App = () => {
   const [userList, setUserList] = useState([]);
+  const [useActivite, setUseActivite] = useState(true);
+
   async function getData() {
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/users',
-    );
+    const response = await axios.get(URL);
+    setUseActivite(false);
     setUserList(response.data);
   }
   const renderItem = ({item}) => <Card cards={item} />;
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <SafeAreaView>
       <View>
         <Text>App</Text>
-        <FlatList data={userList} renderItem={renderItem} />
-        <Button title="GetData" onPress={getData} />
+        {useActivite ? (
+          <ActivityIndicator size={'large'} />
+        ) : (
+          <FlatList data={userList} renderItem={renderItem} />
+        )}
       </View>
     </SafeAreaView>
   );
